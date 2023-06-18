748a60c9f983884c81b987ed7198061bea48c706
rivaldofez <rivaldofez@gmail.com>
2023-06-19 01:35:42 +0700

    add endpoint movie now playing

---
+++ app/src/main/java/com/ykbintang/moviz/data/remote/MovieApi.kt
@@ -0,0 +1,10 @@
+package com.ykbintang.moviz.data.remote
+
+import com.ykbintang.moviz.data.remote.model.MovieListResponse
+import retrofit2.Response
+import retrofit2.http.GET
+
+interface MovieApi {
+    @GET("/movie/now_playing?language=en-US&page=1")
+    suspend fun getMovieNowPlaying(): Response<MovieListResponse>
+}
