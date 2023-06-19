df6dbd6a6abff7fd54aaafdc6074c7b3edd3b2bf
rivaldofez <rivaldofez@gmail.com>
2023-06-19 14:05:37 +0700

    implement bottom navigation bar

---
+++ app/src/main/java/com/ykbintang/moviz/MovizApp.kt
@@ -0,0 +1,113 @@
+package com.ykbintang.moviz
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.ykbintang.moviz.navigation.NavigationItem
+import com.ykbintang.moviz.navigation.Screen
+import com.ykbintang.moviz.ui.screen.HomeScreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MovizApp(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
+    Scaffold(
+        bottomBar = {
+            if (currentRoute != Screen.DetailGame.route){
+                BottomBar(navController = navController)
+            }
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = Screen.Home.route,
+            modifier = Modifier.padding(innerPadding)
+        ){
+            composable(Screen.Home.route) {
+                HomeScreen(navigateToDetail = { gameId ->
+                    navController.navigate(Screen.DetailGame.createRoute(gameId))
+                })
+            }
+        }
+    }
+}
+
+@Composable
+private fun BottomBar(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+){
+    NavigationBar(
+        modifier = modifier
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        val navigationItems = listOf(
+            NavigationItem(
+                title = "Home",
+                icon = Icons.Default.Home,
+                screen = Screen.Home
+            ),
+            NavigationItem(
+                title = "Favorite",
+                icon = Icons.Default.Favorite,
+                screen = Screen.Favorite
+            ),
+            NavigationItem(
+                title = "Profile",
+                icon = Icons.Default.Person,
+                screen = Screen.Profile
+            )
+        )
+
+        NavigationBar {
+            navigationItems.map { navItem ->
+                NavigationBarItem(
+                    icon = {
+                        Icon(
+                            imageVector = navItem.icon,
+                            contentDescription = navItem.title
+                        )
+                    },
+                    label = { Text(navItem.title) },
+                    selected = currentRoute == navItem.screen.route,
+                    onClick = {
+                        navController.navigate(navItem.screen.route) {
+                            popUpTo(navController.graph.findStartDestination().id) {
+                                saveState = true
+                            }
+                            restoreState = true
+                            launchSingleTop = true
+                        }
+                    }
+                )
+            }
+        }
+    }
+}
