32e47b10a97d9fe9a0f26daa9ef05aeb9eb520f1
rivaldofez <rivaldofez@gmail.com>
2023-06-19 13:17:37 +0700

    reformat model item

--- app/src/main/java/com/ykbintang/moviz/model/Movie.kt
+++ app/src/main/java/com/ykbintang/moviz/model/Movie.kt
@@ -4,22 +4,15 @@ import com.google.gson.annotations.SerializedName
 import com.ykbintang.moviz.data.remote.model.MovieListItem
 
 data class Movie(
-    val overview: String? = null,
-    val originalLanguage: String? = null,
-    val originalTitle: String? = null,
-    val video: Boolean? = null,
-    val title: String? = null,
-    val posterPath: String? = null,
+    val originalTitle: String,
+    val title: String,
+    val posterPath: String,
     val backdropPath: String? = null,
-    val releaseDate: String? = null,
-    val popularity: Any? = null,
-    val voteAverage: Any? = null,
-    val id: Int? = null,
-    val adult: Boolean? = null,
-    val voteCount: Int? = null
+    val releaseDate: String,
+    val id: Int,
 )
 
 
 fun MovieListItem.toMovie() = Movie(
-    overview, originalLanguage, originalTitle, video, title, posterPath, backdropPath, releaseDate, popularity
+    originalTitle, title, posterPath, backdropPath, releaseDate, id
 )
