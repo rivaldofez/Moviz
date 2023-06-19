758ba06f875376b302069d82f1668af0c66ccf81
rivaldofez <rivaldofez@gmail.com>
2023-06-19 23:49:17 +0700

    implement detail movie on detail view model

---
+++ app/src/main/java/com/ykbintang/moviz/ui/screen/detail/DetailViewModel.kt
@@ -0,0 +1,42 @@
+package com.ykbintang.moviz.ui.screen.detail
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.ykbintang.moviz.data.MovieRepository
+import com.ykbintang.moviz.model.MovieDetail
+import com.ykbintang.moviz.ui.helper.UiState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class DetailViewModel @Inject constructor(
+    private val movieRepository: MovieRepository
+): ViewModel() {
+    private var _uiState: MutableStateFlow<UiState<MovieDetail>> = MutableStateFlow(UiState.Loading)
+    val uiState: StateFlow<UiState<MovieDetail>> get() = _uiState
+
+    fun getDetailGame(movieId: Int) {
+        try {
+            viewModelScope.launch {
+                movieRepository.getMovieDetail(movieId = movieId)
+                    .catch {
+                        _uiState.value = UiState.Error(it.message.toString())
+                    }
+                    .collect { movieDetail ->
+                        if(movieDetail == null){
+                            _uiState.value = UiState.Error("Empty Data")
+                        } else {
+                            _uiState.value = UiState.Success(movieDetail)
+                        }
+                    }
+            }
+        } catch (e: Exception){
+            _uiState.value = UiState.Error(e.message.toString())
+        }
+    }
+}
