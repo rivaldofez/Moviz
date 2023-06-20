0cfea88b1ba69829609c0bbd9058e806775598fa
rivaldofez <rivaldofez@gmail.com>
2023-06-20 10:30:25 +0700

    add model entity for favorite feature

---
+++ app/src/main/java/com/ykbintang/moviz/data/local/model/MovieDetailEntity.kt
@@ -0,0 +1,30 @@
+package com.ykbintang.moviz.data.local.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.ykbintang.moviz.model.MovieDetail
+
+@Entity(tableName = "movie")
+data class MovieDetailEntity(
+    @PrimaryKey(autoGenerate = false)
+    val id: Int,
+    val originalLanguage: String? = null,
+    val title: String,
+    val backdropPath: String,
+    val genres: String,
+    val voteCount: Int? = null,
+    val overview: String,
+    val originalTitle: String,
+    val runtime: Int,
+    val posterPath: String?,
+    val spokenLanguages: String,
+    val releaseDate: String,
+    val tagline: String? = null,
+    val adult: Boolean? = null,
+    val homepage: String? = null,
+    val status: String? = null
+)
+
+fun MovieDetail.toMovieDetailEntity() = MovieDetailEntity(
+    id, originalLanguage, title, backdropPath, genres, voteCount, overview, originalTitle, runtime, posterPath, spokenLanguages, releaseDate
+)
